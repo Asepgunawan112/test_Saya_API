0250ce839d7d5b10311174a7f3c6a9290007f68e
Asepgunawan112 <ayyasi52@gmail.com>
2026-07-21 07:43:14 +0700

    chore: setup husky v9 configuration

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express = require("express");
-import type { Application, Request, Response } from "express";
+import express = require('express')
+import type { Application, Request, Response } from 'express'
 
-const app: Application = express();
-const port: number = 4000;
+const app: Application = express()
+const port: number = 4000
 
-app.use("/health", (req: Request, res: Response) => {
-  res.status(200);
-  res.send({ status: "200", data: "Server is healthy" });
-});
+app.use('/health', (req: Request, res: Response) => {
+  res.status(200)
+  res.send({ status: '200', data: 'Server is healthy' })
+})
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+  console.log(`Server is running on http://localhost:${port}`)
+})
